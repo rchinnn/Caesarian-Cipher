e32c730927d2f1765f4508a6f7d1f5df25ef5319
Roland Chin <rond24933chn@berkeley.edu>
2018-11-13 23:27:05 -0800

    first commit

--- full-stack-demo/README.md
+++
@@ -1,7 +0,0 @@
-# Caesarian Cipher
-
-## Launch instructions
-
-`export FLASK_APP=main.py`  
-`flask run --port=5005`  
-Access localhost:5005/ on your browser.  
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask(__name__) #initializes the app
 CORS(app) #allows cross-origin sharing (meaning anyone can send requests to the app)
 counter = 0 #this makes counter global (we need this so that when a user changes counter it persists and doesn't get set back to 0 every time).
 code = "hello world"
+
 @app.route('/', methods=['GET'])
 def index():  # pragma: no cover #this loads index.html as the primary web page
     return render_template('index.html')
